de5214688c8a78bf47754afc6a4fa7fba0081967
devbluen <eubluen@gmail.com>
2026-03-16 00:06:07 -0300

    discord delay sendPromotions

--- components/payloads/payload.js
+++ components/payloads/payload.js
@@ -1,11 +1,13 @@
 
 const cache = require("../cache.js");
+const func = require("../functions.js");
 
 async function Payloads(payload, name, client, discordInternal) {
     for (const p of payload) {
         if(p.percent >= process.env.PROMOTION_MINIMUM_VALUE && cache.verify(p.link, p.new_price)) {
             await discordInternal.SendPromotion(client, name.toUpperCase(), p);
             cache.save(p.link, p.new_price);
+            await func.delay(1500);
         }
     }
 }
